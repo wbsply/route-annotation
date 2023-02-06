e509f7ae6420a55d035b1a8468544fc1c94c099e
Soren Malling <soren@meteko.dk>
2023-02-06 14:50:19 +0100

    BUGFIX: Add missing argument annotations

--- Classes/Annotations/Route.php
+++ Classes/Annotations/Route.php
@@ -4,6 +4,10 @@ namespace WebSupply\RouteAnnotation\Annotations;
 
 use Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor;
 
+/**
+* @Annotation
+* @NamedArgumentConstructor
+*/
 #[\Attribute(\Attribute::TARGET_METHOD|\Attribute::TARGET_CLASS)]
 final class Route
 {
